70e42ba504d834c8d300447ce8fdd86b6bc0ced7
pande <pandiksanjaya28@gmail.com>
2023-10-06 21:15:36 +0700

    last baru

--- src/app.js
+++ src/app.js
@@ -2,18 +2,33 @@ new Vue({
     el: '#app',
     data: {
         name: '',
-        height: '',
-        idealWeight: null
+        height: null,
+        calculationType: 'weight',
+        idealWeight: null,
+        savedResults: [], // Array to store saved results
     },
     methods: {
-        calculateWeight: function() {
-            if (this.height > 0) {
-                // Calculate ideal weight (for example, using a simple formula)
-                // You can replace this formula with a more accurate one as needed.
-                this.idealWeight = Math.round((this.height - 100) * 0.9);
-            } else {
+        calculateWeight: function () {
+            if (this.height) {
+                // Calculate ideal weight based on height
+                this.idealWeight = this.height - 100;
+            }
+        },
+        saveResult: function () {
+            if (this.idealWeight !== null) {
+                // Save the ideal weight result
+                this.savedResults.push({
+                    name: this.name,
+                    idealWeight: this.idealWeight,
+                });
+                // Reset name and idealWeight for the next calculation
+                this.name = '';
                 this.idealWeight = null;
             }
-        }
-    }
+        },
+        removeResult: function (index) {
+            // Remove a result from the savedResults array
+            this.savedResults.splice(index, 1);
+        },
+    },
 });
